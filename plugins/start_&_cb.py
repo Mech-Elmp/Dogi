cfecced58e4737d37b23a6e07e5c6c06dcb7a7aa
Mech-Elmp <119470972+Mech-Elmp@users.noreply.github.com>
2024-05-28 17:46:54 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -82,6 +82,64 @@ async def myplan(client, message):
         await m.delete()
 
 @Client.on_message(filters.private & filters.command("plans"))
+import random, asyncio, datetime, pytz, time, psutil, shutil
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from helper.database import db
+from config import Config, rkn
+from helper.utils import humanbytes
+
+# Inline keyboard buttons
+upgrade_button = InlineKeyboardMarkup([
+    [InlineKeyboardButton('Buy Premium ✓', callback_data='buy_premium')],
+    [InlineKeyboardButton("Back", callback_data="start")]
+])
+
+upgrade_trial_button = InlineKeyboardMarkup([
+    [InlineKeyboardButton('Buy Premium ✓', callback_data='buy_premium')],
+    [InlineKeyboardButton("Trial - 12 Hours ✓", callback_data="give_trial")],
+    [InlineKeyboardButton("Back", callback_data="start")]
+])
+
+start_button = InlineKeyboardMarkup([
+    [InlineKeyboardButton('Updates', url='https://t.me/Digital_Botz'),
+     InlineKeyboardButton('Support', url='https://t.me/DigitalBotz_Support')],
+    [InlineKeyboardButton('About', callback_data='about'),
+     InlineKeyboardButton('Help', callback_data='help')],
+    [InlineKeyboardButton('💸 Upgrade to Premium 💸', callback_data='upgrade')]
+])
+
+# Start command handler
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    await db.add_user(client, message) 
+    if Config.RKN_PIC:
+        await message.reply_photo(Config.RKN_PIC, caption=rkn.START_TXT.format(user.mention), reply_markup=start_button)       
+    else:
+        await message.reply_text(text=rkn.START_TXT.format(user.mention), reply_markup=start_button, disable_web_page_preview=True)
+
+# Myplan command handler
+@Client.on_message(filters.private & filters.command("myplan"))
+async def myplan(client, message):
+    user_id  = message.from_user.id
+    user = message.from_user.mention
+    if await db.has_premium_access(user_id):
+        data = await db.get_user(user_id)
+        expiry_time = data.get("expiry_time")
+        expiry_str_in_ist = datetime.datetime.strptime(expiry_time, "%Y-%m-%d %H:%M:%S")
+        time_left_str = expiry_str_in_ist - datetime.datetime.now()
+        await message.reply_text(f"⚜️ Your plan details are:\n\n👤 User: {user}\n⚡ User ID: <code>{user_id}</code>\n⏰ Time Left: {time_left_str}\n⌛️ Expiry Date: {expiry_str_in_ist}")
+    else:
+        m = await message.reply_sticker("CAACAgIAAxkBAAIBTGVjQbHuhOiboQsDm35brLGyLQ28AAJ-GgACglXYSXgCrotQHjibHgQ")
+        await message.reply_text(
+            f"Hey {user},\n\nYou do not have any active premium plans. If you want to take premium then click on the below button 👇",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💸 Checkout Premium Plans 💸", callback_data='upgrade')]]))			 
+        await asyncio.sleep(2)
+        await m.delete()
+
+# Plans command handler
+@Client.on_message(filters.private & filters.command("plans"))
 async def plans(client, message):
     user = message.from_user
     free_trial_status = await db.get_free_trial_status(user.id)
@@ -92,8 +150,8 @@ async def plans(client, message):
             await message.reply_text(text=rkn.UPGRADE.format(user.mention), reply_markup=upgrade_button, disable_web_page_preview=True)
     else:
         await message.reply_text(text=rkn.UPGRADE.format(user.mention), reply_markup=upgrade_button, disable_web_page_preview=True)
-   
-  
+
+# Callback query handler
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
     data = query.data 
@@ -101,41 +159,37 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=rkn.START_TXT.format(query.from_user.mention),
             disable_web_page_preview=True,
-            reply_markup = start_button)
+            reply_markup=start_button)
         
     elif data == "help":
         await query.message.edit_text(
             text=rkn.HELP_TXT,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                #⚠️ don't change source code & source link ⚠️ #
-                InlineKeyboardButton("ᴛʜᴜᴍʙɴᴀɪʟ", callback_data = "thumbnail"),
-                InlineKeyboardButton("ᴄᴀᴘᴛɪᴏɴ", callback_data = "caption")
+                InlineKeyboardButton("Thumbnail", callback_data="thumbnail"),
+                InlineKeyboardButton("Caption", callback_data="caption")
                 ],[          
-                InlineKeyboardButton("ᴄᴜsᴛᴏᴍ ғɪʟᴇ ɴᴀᴍᴇ", callback_data = "custom_file_name")    
+                InlineKeyboardButton("Custom File Name", callback_data="custom_file_name")    
                 ],[          
-                InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data = "about"),
-                InlineKeyboardButton("ᴍᴇᴛᴀᴅᴀᴛᴀ", callback_data = "digital_meta_data")
-                                     ],[
-                InlineKeyboardButton("Bᴀᴄᴋ", callback_data = "start")
+                InlineKeyboardButton("About", callback_data="about"),
+                InlineKeyboardButton("Metadata", callback_data="digital_meta_data")
+                ],[
+                InlineKeyboardButton("Back", callback_data="start")
                   ]]))         
         
     elif data == "about":
         await query.message.edit_text(
-            text=rkn.ABOUT_TXT.format(client.mention),
-            disable_web_page_preview = True,
+            text=rkn.ABOUT_TXT.format(query.from_user.mention),
+            disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-                #⚠️ don't change source code & source link ⚠️ #
-                InlineKeyboardButton("𝚂ᴏᴜʀᴄᴇ",
-     callback_data = "source_code"), #Whoever is deploying this repo is given a warning ⚠️ not to remove this repo link #first & last warning ⚠️
-                InlineKeyboardButton("ʙᴏᴛ sᴛᴀᴛᴜs", callback_data = "bot_status")
+                InlineKeyboardButton("Source", callback_data="source_code"),
+                InlineKeyboardButton("Bot Status", callback_data="bot_status")
                 ],[
-                InlineKeyboardButton("ʟɪᴠᴇ sᴛᴀᴛᴜs", callback_data = "live_status"),
-                InlineKeyboardButton("ᴜᴘɢʀᴀᴅᴇ", callback_data = "upgrade")
+                InlineKeyboardButton("Live Status", callback_data="live_status"),
+                InlineKeyboardButton("Upgrade", callback_data="upgrade")
                 ],[   
-                InlineKeyboardButton("Bᴀᴄᴋ", callback_data = "start")
-            
-           ]]))    
+                InlineKeyboardButton("Back", callback_data="start")
+                ]]))    
         
     elif data == "upgrade":
         free_trial_status = await db.get_free_trial_status(query.from_user.id)
@@ -152,9 +206,9 @@ async def cb_handler(client, query: CallbackQuery):
         free_trial_status = await db.get_free_trial_status(query.from_user.id)
         if not free_trial_status:            
             await db.give_free_trail(query.from_user.id)
-            new_text = "**ʏᴏᴜʀ ᴘʀᴇᴍɪᴜᴍ ᴛʀɪᴀʟ ʜᴀs ʙᴇᴇɴ ᴀᴅᴅᴇᴅ ғᴏʀ 𝟷𝟸 ʜᴏᴜʀs.\n\nʏᴏᴜ ᴄᴀɴ ᴜsᴇ ꜰʀᴇᴇ ᴛʀᴀɪʟ ꜰᴏʀ 𝟷𝟸 ʜᴏᴜʀs ꜰʀᴏᴍ ɴᴏᴡ 😀\n\nआप अब से 𝟷𝟸 घण्टा के लिए निःशुल्क ट्रायल का उपयोग कर सकते हैं 😀**"
+            new_text = "**Your premium trial has been added for 12 hours.\n\nYou can use free trial for 12 hours from now 😀\n\nआप अब से 12 घंटे के लिए निःशुल्क ट्रायल का उपयोग कर सकते हैं 😀**"
         else:
-            new_text = "**🤣 ʏᴏᴜ ᴀʟʀᴇᴀᴅʏ ᴜsᴇᴅ ғʀᴇᴇ ɴᴏᴡ ɴᴏ ᴍᴏʀᴇ ғʀᴇᴇ ᴛʀᴀɪʟ. ᴘʟᴇᴀsᴇ ʙᴜʏ sᴜʙsᴄʀɪᴘᴛɪᴏɴ ʜᴇʀᴇ ᴀʀᴇ ᴏᴜʀ 👉 /plans**"
+            new_text = "**🤣 You already used free trial. Now no more free trial. Please buy subscription here 👉 /plans**"
         await client.send_message(query.from_user.id, text=new_text)
 
     elif data == "thumbnail":
@@ -162,28 +216,28 @@ async def cb_handler(client, query: CallbackQuery):
             text=rkn.THUMBNAIL,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton(" Bᴀᴄᴋ", callback_data = "help")]])) 
+             InlineKeyboardButton("Back", callback_data="help")]])) 
       
     elif data == "caption":
         await query.message.edit_text(
             text=rkn.CAPTION,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton(" Bᴀᴄᴋ", callback_data = "help")]])) 
+             InlineKeyboardButton("Back", callback_data="help")]])) 
       
     elif data == "custom_file_name":
         await query.message.edit_text(
             text=rkn.CUSTOM_FILE_NAME,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton(" Bᴀᴄᴋ", callback_data = "help")]])) 
+             InlineKeyboardButton("Back", callback_data="help")]])) 
       
     elif data == "digital_meta_data":
         await query.message.edit_text(
             text=rkn.DIGITAL_METADATA,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton(" Bᴀᴄᴋ", callback_data = "help")]])) 
+             InlineKeyboardButton("Back", callback_data="help")]])) 
       
     elif data == "bot_status":
         total_users = await db.total_users_count()
@@ -195,7 +249,7 @@ async def cb_handler(client, query: CallbackQuery):
             text=rkn.BOT_STATUS.format(uptime, total_users, total_premium_users, sent, recv),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton(" Bᴀᴄᴋ", callback_data = "about")]])) 
+             InlineKeyboardButton("Back", callback_data="about")]])) 
       
     elif data == "live_status":
         currentTime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - client.uptime))    
@@ -212,7 +266,7 @@ async def cb_handler(client, query: CallbackQuery):
             text=rkn.LIVE_STATUS.format(currentTime, cpu_usage, ram_usage, total, used, disk_usage, free, sent, recv),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([[
-             InlineKeyboardButton(" Bᴀᴄᴋ", callback_data = "about")]])) 
+             InlineKeyboardButton("Back", callback_data="about")]])) 
       
     elif data == "source_code":
         await query.message.edit_text(
