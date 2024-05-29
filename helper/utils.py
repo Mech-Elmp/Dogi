e6e37d3457fc0acedde95fcff5ee00b96a0fb742
Mech-Elmp <119470972+Mech-Elmp@users.noreply.github.com>
2024-05-29 17:17:35 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -18,7 +18,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
     if round(diff % 5.00) == 0 or current == total:        
         percentage = current * 100 / total
         speed = current / diff
-        displayed_speed = speed + 2  # Increase the displayed speed by 2 units
+        displayed_speed = speed + 10  # Increase the displayed speed by 2 units
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
